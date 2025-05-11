8166eb07b07064ff8c9a5142c460f6bcd3052cb6
Marley William Byers <professionalmarley@gmail.com>
2025-05-11 02:09:38 -0700

    Added backgrounds to existing levels

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Sprite backgroundSprite;
+    [SerializeField]
+    private Material unlitMat;
+
     void Start()
     {
         CinemachineBrain brain = GetComponent<CinemachineBrain>();
@@ -15,12 +18,28 @@ public class CameraController : MonoBehaviour
             return;
         }
         StartCoroutine(GetCamera(brain));
-        GetComponentInChildren<SpriteRenderer>().sprite = backgroundSprite;
-        if (backgroundSprite == null)
+        GameObject bg;
+        SpriteRenderer bgSR = GetComponentInChildren<SpriteRenderer>();
+        if (bgSR != null)
         {
-            Debug.LogError("Skybox material not assigned.");
-            return;
+            bg = GetComponentInChildren<SpriteRenderer>().gameObject;
         }
+        else
+        {
+            bg = new GameObject("Background");
+            bg.transform.SetParent(transform);
+            bg.transform.localPosition = Vector3.forward;
+            bg.transform.localScale = new Vector3(1.85f, 1.85f, 1);
+            bgSR = bg.AddComponent<SpriteRenderer>();
+            if (backgroundSprite == null || unlitMat == null)
+            {
+                Debug.LogError("Skybox material or unlitMat not assigned.");
+                return;
+            }
+            bgSR.sprite = backgroundSprite;
+            bgSR.material = unlitMat;
+        }
+        bgSR.sortingLayerName = "Sky";
     }
 
     private IEnumerator GetCamera(CinemachineBrain brain)
